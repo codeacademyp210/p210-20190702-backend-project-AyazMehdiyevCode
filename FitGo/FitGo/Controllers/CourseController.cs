a48667d39e7b5609a30d0abd0ad8ac9ed8fbb288
AyazMehdiyevCode <46813361+AyazMehdiyevCode@users.noreply.github.com>
2019-07-09 23:54:12 +0400

    c

---
+++ FitGo/FitGo/Controllers/CourseController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FitGo.Controllers
+{
+    public class CourseController : Controller
+    {
+        // GET: Course
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Schedule()
+        {
+            return View();
+        }
+
+        public ActionResult Rooms()
+        {
+            return View();
+        }
+
+        public ActionResult Trainers()
+        {
+            return View();
+        }
+    }
+}
